38fdc6e908e86a8cd619e519c2685cfb98574d27
Mike Zhylevych <mzhylevych@chegg.com>
2019-08-25 12:23:30 +0300

    current project

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export const dateStringToDate = (dateString: string): Date => {
+  const dateParts = dateString.split('/').map(
+    (val: string) => {
+      return parseInt(val);
+    }
+  );
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+}
